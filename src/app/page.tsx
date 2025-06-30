a150a9f5bf49cc1593e03b82f5b7f79e6e1d428e
sahilsama <shahilbudakoti07@gmail.com>
2025-06-30 23:58:04 +0530

    updated the sign and signed out button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 // import { SignInButton } from '@clerk/nextjs'
+"use client"
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
       homepage
-      
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
     </div>
   )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,13 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+// protecting the profile withot login
+const isProtectedRoute = createRouteMatcher(["/generate-program", "/profile"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if(isProtectedRoute(req)) await auth.protect();
+});
+
+
 
 export const config = {
   matcher: [
